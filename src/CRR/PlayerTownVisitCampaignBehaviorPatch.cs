9ca68f248d652e0de640a3449fdad18a11d2b178
qinjian.li <liqinjian999@163.com>
2025-08-05 21:15:51 +0800

    CRR.

--- src/CRR/PlayerTownVisitCampaignBehaviorPatch.cs
+++ src/CRR/PlayerTownVisitCampaignBehaviorPatch.cs
@@ -3,6 +3,7 @@ using TaleWorlds.CampaignSystem.CampaignBehaviors;
 using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterDeveloper;
 
 namespace FlavorCraft
 {
@@ -16,19 +17,18 @@ namespace FlavorCraft
             if (Statics._settings is null || !Statics._settings.EnableCRR)
                 return true;
 
-            // 检查玩家是否独立或village属于叛军
+            // 检查玩家是否独立或据点属于叛军
             bool isPlayerIndependent = Hero.MainHero.Clan.Kingdom == null;
             bool isRebelClan = Settlement.CurrentSettlement?.OwnerClan?.Kingdom?.IsRebelClan ?? false;
 
-            // 独立玩家或叛军village可自由访问
+            // 独立玩家或叛军可自由访问
             if (isPlayerIndependent || isRebelClan)
             {
-                return true; 
+                return true;
             }
 
-            // 检查villiage是否属于其他阵营
-            bool isEnemySettlement = Settlement.CurrentSettlement?.OwnerClan.Kingdom.StringId != Hero.MainHero.Clan?.Kingdom?.StringId;
-            if (isEnemySettlement)
+            // 检查是否属于敌对阵营
+            if (Settlement.CurrentSettlement != null && Settlement.CurrentSettlement.MapFaction.IsAtWarWith(Hero.MainHero.MapFaction))
             {
                 __result = false;
                 return false;
@@ -44,19 +44,18 @@ namespace FlavorCraft
             if (Statics._settings is null || !Statics._settings.EnableCRR)
                 return true;
 
-            // 检查玩家是否独立或town属于叛军
+            // 检查玩家是否独立或据点属于叛军
             bool isPlayerIndependent = Hero.MainHero.Clan.Kingdom == null;
             bool isRebelClan = Settlement.CurrentSettlement?.OwnerClan?.Kingdom?.IsRebelClan ?? false;
 
-            // 独立玩家或叛军town可自由访问
+            // 独立玩家或叛军可自由访问
             if (isPlayerIndependent || isRebelClan)
             {
                 return true;
             }
 
-            // 检查town是否属于其他阵营
-            bool isEnemySettlement = Settlement.CurrentSettlement?.OwnerClan.Kingdom.StringId != Hero.MainHero.Clan?.Kingdom?.StringId;
-            if (isEnemySettlement)
+            // 检查是否属于敌对阵营
+            if (Settlement.CurrentSettlement != null && Settlement.CurrentSettlement.MapFaction.IsAtWarWith(Hero.MainHero.MapFaction))
             {
                 __result = false;
                 return false;
